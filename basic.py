afa4098e94af63c5dd467b8b68b5b2f676be5bc0
Aravind <baravind031@gmail.com>
2023-09-08 09:47:37 +0530

    first code

--- basic.py
+++ basic.py
@@ -1,3 +1,12 @@
 print("hello aravind")
 
 print("hey Aravind !")
+
+def even(num):
+    if num%2 == 0:
+        return "even"
+    else:
+        return "odd"
+num = int(input("enter a number"))
+res = even(num)
+print(res)
